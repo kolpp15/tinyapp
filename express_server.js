86892dd0622d8150164bd7735fab326025463090
Brian Sohn <kolpp15@gmail.com>
2021-08-02 16:25:59 -0700

    setting ejs as the view engine

--- express_server.js
+++ express_server.js
@@ -4,6 +4,7 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.use(morgan("dev"));
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
