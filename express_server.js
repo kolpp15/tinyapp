b2f0f11707e4fc532b2548253b1e25b2217ce08c
Brian Sohn <kolpp15@gmail.com>
2021-08-02 14:25:59 -0700

    sending HTML code and fetching with cURL -i

--- express_server.js
+++ express_server.js
@@ -3,6 +3,8 @@ const morgan = require("morgan");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.use(morgan("dev"));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -14,6 +16,10 @@ app.get("/", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
