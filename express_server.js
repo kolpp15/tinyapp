6a9692bab1d0b86749dccfb56b17b4bd4f559b16
Brian Sohn <kolpp15@gmail.com>
2021-08-04 23:46:12 -0700

    Additional permission features: users can only see their own URLs and edit them.

--- express_server.js
+++ express_server.js
@@ -28,19 +28,31 @@ const urlDatabase = {
 const users = {
   "userRandomID": {
     id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur"
+    email: "b@b.com",
+    password: "1234"
   },
   "user2RandomID": {
     id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk"
+    email: "c@c.com",
+    password: "1234"
   }
 };
 
 // Create random strings
 const generateRandomString = function(length = 6) {
   return Math.random().toString(20).substr(2, length);
+};
+
+// Function: returns the URLS where the userID is equal to the id of current logged-in user
+// Loop through urlDatabase (forin), match id & urlDatabaseID, if match return object
+const urlsForUser = (id) => {
+  const userObj = {};
+  for (const data in urlDatabase) {
+    if (id === urlDatabase[data].userID) {
+      userObj[data] = urlDatabase[data];
+    }
+  }
+  return userObj;
 };
 
 // ------------------------------------------------------------------------ LOGIN
@@ -118,12 +130,12 @@ app.post("/register", (req, res) => {
 
 // list of urls in the database object
 app.get("/urls", (req, res) => {
+  const userDatabase = urlsForUser(req.cookies.user_id);
   const templateVars = {
-    urls: urlDatabase,
+    urls: userDatabase,
     "user_id": req.cookies.user_id,
     users: users
-  }; // refer to the above object. This can also be a direct object
-  
+  };
   res.render("urls_index", templateVars); // for Express, it automatically searches in the view file with .ejs
 });
 
@@ -145,13 +157,13 @@ app.post("/urls", (req, res) => {
   console.log(req.body);
 
   if (req.cookies.user_id === undefined) {
-    res.status(403);
+    res.status(404);
     res.send("Please login first!");
   } else {
     const newShortURL = generateRandomString();
     urlDatabase[newShortURL] = {
       "longURL": req.body.longURL,
-      "userID": req.body.userID
+      "userID": req.cookies.user_id
     }; // the value of the new short URL ID/key
   
     res.redirect(`/urls/${newShortURL}`);
@@ -160,16 +172,18 @@ app.post("/urls", (req, res) => {
 
 // parameter based on the database ID
 app.get("/urls/:shortURL", (req, res) => {
+  const userDatabase = urlsForUser(req.cookies.user_id);
+  const userShortUrl = req.params.shortURL;
   
-  if (req.cookies.user_id === undefined) {
-    res.status(403);
-    res.send("Please login first!");
+  //validate user with registered url
+  if (userDatabase[userShortUrl] === undefined) {
+    res.status(404);
+    res.send(`<html><body><p><b> Please <a href='/login'>log in</a> with a valid account. If not registered, click <a href='/register'>register</a> here!<b></body></html>`);
   } else {
     const templateVars = {
       shortURL: req.params.shortURL,
       longURL: urlDatabase[req.params.shortURL].longURL,
       "user_id": req.cookies.user_id,
-      users: users
     };
     res.render("urls_show", templateVars);
   }
@@ -188,6 +202,12 @@ app.get("/u/:shortURL", (req, res) => {
 // Edit / POST /urls/:shortURL
 app.post("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
+
+  if (req.cookies.user_id !== urlDatabase[shortURL].userID) {
+    res.status(404);
+    res.send('You are not authorized to Edit!');
+  }
+
   const newLongURL = req.body.updateURL; // body parser in express //this creates a new value
   urlDatabase[shortURL].longURL = newLongURL;
   res.redirect("/urls");
@@ -196,6 +216,11 @@ app.post("/urls/:shortURL", (req, res) => {
 // DELETE / POST /urls/:shortURL/delete
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL;
+
+  if (req.cookies.user_id !== urlDatabase[shortURL].userID) {
+    res.status(404);
+    res.send('You are not authorized to delete!');
+  }
   delete urlDatabase[shortURL];
   res.redirect("/urls");
 });
